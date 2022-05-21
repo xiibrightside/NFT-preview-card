c4d4920667d310702bb19a48a5b92e3680cf7e76
xoxo <thakursatyam48@gmail.com>
2022-05-21 21:48:39 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import equi from "./images/image-equilibrium.jpg";
+import iconeth from "./images/icon-ethereum.svg";
+import iconclock from "./images/icon-clock.svg";
+import avatar from "./images/image-avatar.png";
+import prev from "./images/icon-view.svg";
 
 function App() {
+  const [isPrev, setIsPrev] = useState(false);
+
+  const Preview = () => {
+    return (
+      <div onMouseLeave={() => setIsPrev(false)} className="prev">
+        <img className="preview" src={prev} alt="preview"></img>
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <body>
+      <div className="card">
+        <div className="heroimgs">
+          <img
+            onMouseOver={() => setIsPrev(true)}
+            className="equi mx-auto"
+            src={equi}
+            alt="equilibrium"
+          ></img>
+          {isPrev && <Preview />}
+        </div>
+        <h1>
+          <a className="equititle" href="#0">
+            Equilibrium #3429
+          </a>
+        </h1>
+        <p className="details">
+          Our Equilibrium collection promotes balance and calm.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="ethinfo">
+          <img className="iconeth" src={iconeth} alt="ethereum"></img>
+          <h2>0.041 ETH</h2>
+          <img className="iconclock" src={iconclock} alt="clock"></img>
+          <p className="days">3 days left</p>
+        </div>
+        <div className="rect mx-auto"></div>
+        <div className="creator">
+          <img className="avatar" src={avatar} alt="avatar"></img>
+          <p id="creator">
+            Creation of{" "}
+            <a className="jules" href="#0">
+              Jules Wyvern
+            </a>
+          </p>
+        </div>
+      </div>
+    </body>
   );
 }
 
